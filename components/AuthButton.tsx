2f3726ce267161ed04af2b4374142bf5b29ff973
DarshanJain-Techuz <darshan@techuz.com>
2024-05-03 11:57:47 +0530

    Adding user email if user full name is not available

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -17,11 +17,10 @@ export default async function AuthButton() {
   const {
     data: { user }
   } = await supabase.auth.getUser();
-
   return user ? (
     <>
       <div className="flex items-center gap-4">
-        Hey, {user.user_metadata.name}
+        Hey, {user.user_metadata.name || user.email}
         <Link href="/update-profile" className="text-gray-800 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">Update Profile</Link>
         <form action={signOut}>
           <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
