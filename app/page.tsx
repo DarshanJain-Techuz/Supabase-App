7b21240d2669c1f08163f94b8b280138f850ffa0
DarshanJain-Techuz <darshan@techuz.com>
2024-05-06 15:25:14 +0530

    Adding dashboard route for authenticated user.

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,42 @@
+'use client'
 import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
+import { createClient } from "@/utils/supabase/client";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { User } from "@supabase/supabase-js";
 
-export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
+export default function Index() {
+  const supabase = createClient()
+  const [user, setUser] = useState<User | null>()
+
+  useEffect(() => {
+    async function getUser() {
+      const {
+        data: { user }
+      } = await supabase.auth.getUser();
+      setUser(user)
     }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
+    getUser()
+  }, [supabase])
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <Link href='/'><Image src='/Logo.svg' alt='company logo' width={150} height={150} /></Link>
-          {isSupabaseConnected && <AuthButton />}
+          {user && <Link href='/dashboard'>dashboard</Link>}
+          <AuthButton />
         </div>
-      </nav>
+      </nav >
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+          <SignUpUserSteps />
         </main>
       </div>
 
